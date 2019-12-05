dcca150ff38d3667c118f12ff844460a51cca5fa
EW <ewong@admins-MacBook-Pro.local>
2019-12-04 21:03:10 -0800

    feed fragment

--- StorySharing/app/src/main/java/com/example/storysharing/ui/feed/FeedFragment.java
+++ StorySharing/app/src/main/java/com/example/storysharing/ui/feed/FeedFragment.java
@@ -1,26 +1,22 @@
-package com.example.storysharing.ui.home;
+package com.example.storysharing.ui.feed;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.storysharing.R;
 
-public class HomeFragment extends Fragment {
+public class FeedFragment extends Fragment {
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_feed, container, false);
         return root;
     }
 }
