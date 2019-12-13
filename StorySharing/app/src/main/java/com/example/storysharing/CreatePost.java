f01483dacd14b0ba2f899c150ca46ffa01e80dcc
EW <ewong@admins-MacBook-Pro.local>
2019-12-12 20:37:08 -0800

    limit title length

--- StorySharing/app/src/main/java/com/example/storysharing/CreatePost.java
+++ StorySharing/app/src/main/java/com/example/storysharing/CreatePost.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,23 +33,52 @@ public class CreatePost extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String titleText = title.getText().toString();
-                String bodyText = body.getText().toString();
-                boolean isAnon = anon.isChecked();
-                String email;
+                if (validateForm()) {
+                    String titleText = title.getText().toString();
+                    String bodyText = body.getText().toString();
+                    boolean isAnon = anon.isChecked();
+                    String email;
 
-                if (isAnon) {
-                    email = "Anonymous";
-                } else {
-                    email = mAuth.getCurrentUser().getEmail();
+                    if (isAnon) {
+                        email = "Anonymous";
+                    } else {
+                        email = mAuth.getCurrentUser().getEmail();
+                    }
+
+                    FirebaseUtility.saveNewPost(titleText, bodyText, email, isAnon);
+
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
                 }
-
-                FirebaseUtility.saveNewPost(titleText, bodyText, email, isAnon);
-
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(intent);
             }
         });
 
+    }
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        EditText title = findViewById(R.id.challenge_title);
+
+        if (TextUtils.isEmpty(title.getText().toString())) {
+            title.setError("Required.");
+            valid = false;
+        } else if (title.getText().length() > 50) {
+            title.setError("Max 50 characters");
+        } else {
+            title.setError(null);
+        }
+
+
+        EditText description = findViewById(R.id.description_input);
+        if (TextUtils.isEmpty(description.getText().toString())) {
+            description.setError("Required.");
+            valid = false;
+        } else {
+            description.setError(null);
+        }
+
+        return valid;
     }
 }
